2b966bc3582de22ebe723d3e638b4d0966db435b
zyssnh <1123611308@qq.com>
2026-06-03 20:32:46 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        navigateFallbackDenylist: [/^\/docs\//],
       }
     })
   ]
